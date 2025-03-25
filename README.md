6940bd5ca3001576ce2ac9a3344b002710463cd8
Michael Asnake <105238111+michael-dukka@users.noreply.github.com>
2025-03-25 11:08:50 +0300

    chore: Create flutter project 🎉

---
+++ README.md
@@ -0,0 +1,3 @@
+# lot_flutter
+
+This repo contains the flutter project for the LOT app which consumes the LOT API.
---
+++ android/app/src/main/kotlin/com/michael_asnake/lot_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.michael_asnake.lot_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
